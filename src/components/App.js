3e01d0071cef07282e849e51afd58ff0b5ef1d5b
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-07-30 13:10:56 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import WeatherDisplay from "./WeatherDisplay";
+import "./../styles/App.css"; // Optional
 
 const App = () => {
+  const [weather, setWeather] = useState({
+    temperature: 25,
+    conditions: "Sunny",
+  });
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <WeatherDisplay weather={weather} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
